16982c81df251ae8888d7f0ab8a63334ad01b92a
Roberto Cestari <robertotcestari@gmail.com>
2024-06-04 10:33:57 -0300

    adjust layout

--- components/drawer-dialog.tsx
+++ components/drawer-dialog.tsx
@@ -66,7 +66,9 @@ export function DrawerDialog() {
             Cadastre um pedido. Clique em salvar quando terminar.
           </DrawerDescription>
         </DrawerHeader>
-        <OrderForm />
+        <div className='p-4'>
+          <OrderForm />
+        </div>
         <DrawerFooter className="pt-2">
           <DrawerClose asChild>
             <Button variant="outline">Cancelar</Button>
